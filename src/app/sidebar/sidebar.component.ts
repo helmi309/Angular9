e3ef79b91058057976e03784a686d78295d8bc25
helmi309 <helmiyachyaku@gmail.com>
2020-05-31 09:01:16 +0700

    update sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,13 +9,13 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
-    { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
-    { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+    // { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
+    // { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
     { path: '/siswa', title: 'Siswa List',  icon:'pe-7s-user', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+    // { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
+    // { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
+    // { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
+    // { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
 ];
 
 @Component({
